090bbe4a8da19bff2be31c2e68badc5fcfceb8db
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-11 16:41:34 -0800

    Created bubbleSort Algorithm

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let madeSwap = false;
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        madeSwap = true;
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    if (madeSwap === false) return arr;
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
